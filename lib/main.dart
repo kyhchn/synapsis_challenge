7ade496c447724eae7a6c94b9dcddfa1cfb1a7b1
kyhchn <kyaahh69@gmail.com>
2023-10-19 06:45:55 +0700

    `refactor` wrap with multi bloc provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:sizer/sizer.dart';
 import 'package:synapsis_challenge/config/colors.dart';
-import 'package:synapsis_challenge/pages/login_view.dart';
+import 'package:synapsis_challenge/features/login/presentation/bloc/login_bloc.dart';
+import 'package:synapsis_challenge/features/login/presentation/login_view.dart';
+import 'package:synapsis_challenge/features/survei/presentation/bloc/home_bloc.dart';
+import 'package:synapsis_challenge/injection_container.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDependencies();
   runApp(const MyApp());
 }
 
@@ -14,38 +20,49 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Sizer(
-      builder: (context, orientation, deviceType) => MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(
-              seedColor: SynapsisColor.primaryColor, background: Colors.white),
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          fontFamily: 'Inter',
-          inputDecorationTheme: InputDecorationTheme(
-            enabledBorder: const OutlineInputBorder(
-              borderSide: BorderSide(width: 1, color: Color(0xFFD6E4EC)),
-              borderRadius: BorderRadius.all(Radius.circular(4)),
-            ),
-            focusedBorder: const OutlineInputBorder(
-              borderSide:
-                  BorderSide(width: 2, color: SynapsisColor.primaryColor),
-              borderRadius: BorderRadius.all(Radius.circular(4)),
-            ),
-            errorBorder: OutlineInputBorder(
-              borderSide: BorderSide(width: 1, color: Colors.red.shade300),
-              borderRadius: const BorderRadius.all(Radius.circular(10)),
-            ),
-            focusedErrorBorder: OutlineInputBorder(
-              borderSide: BorderSide(width: 2, color: Colors.red.shade300),
-              borderRadius: const BorderRadius.all(Radius.circular(10)),
-            ),
-            contentPadding:
-                EdgeInsets.symmetric(vertical: 1.h, horizontal: 2.h),
-            suffixIconColor: const Color(0xFF9DA7AD),
+      builder: (context, orientation, deviceType) => MultiBlocProvider(
+        providers: [
+          BlocProvider<LoginBloc>(
+            create: (context) => sl()..add(CheckLocalUser()),
           ),
-          useMaterial3: true,
+          BlocProvider<HomeBloc>(
+            create: (context) => sl(),
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(
+                seedColor: SynapsisColor.primaryColor,
+                background: Colors.white),
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            fontFamily: 'Inter',
+            inputDecorationTheme: InputDecorationTheme(
+              enabledBorder: const OutlineInputBorder(
+                borderSide: BorderSide(width: 1, color: Color(0xFFD6E4EC)),
+                borderRadius: BorderRadius.all(Radius.circular(4)),
+              ),
+              focusedBorder: const OutlineInputBorder(
+                borderSide:
+                    BorderSide(width: 2, color: SynapsisColor.primaryColor),
+                borderRadius: BorderRadius.all(Radius.circular(4)),
+              ),
+              errorBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 1, color: Colors.red.shade300),
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+              ),
+              focusedErrorBorder: OutlineInputBorder(
+                borderSide: BorderSide(width: 2, color: Colors.red.shade300),
+                borderRadius: const BorderRadius.all(Radius.circular(10)),
+              ),
+              contentPadding:
+                  EdgeInsets.symmetric(vertical: 1.h, horizontal: 2.h),
+              suffixIconColor: const Color(0xFF9DA7AD),
+            ),
+            useMaterial3: true,
+          ),
+          home: const LoginView(),
         ),
-        home: const LoginView(),
       ),
     );
   }
