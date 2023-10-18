77bf39adaee6c241a50ec7069bdcd600a98e091c
kyhchn <kyaahh69@gmail.com>
2023-10-19 06:45:15 +0700

    `feat` add cache service

---
+++ lib/services/user_cache_services.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:synapsis_challenge/features/login/data/models/user.dart';
+import 'package:synapsis_challenge/injection_container.dart';
+
+class UserCacheService {
+  UserModel? _user;
+
+  UserModel? get user => _user;
+  SharedPreferences get sharedPrefs => sl<SharedPreferences>();
+
+  Future<void> saveUser(UserModel user) async {
+    _user = user;
+    await sharedPrefs.setString('user', jsonEncode(user.toJson()));
+  }
+
+  UserModel? getUser() {
+    final userString = sharedPrefs.getString('user');
+    print(userString);
+    if (userString != null) {
+      _user = UserModel.fromJson(jsonDecode(userString));
+      return _user;
+    }
+    return null;
+  }
+
+  Future<void> deleteUser() async {
+    _user = null;
+    await sharedPrefs.remove('user');
+  }
+}
